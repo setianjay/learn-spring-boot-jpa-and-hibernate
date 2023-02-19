e0a889c04f6dfbd2cc5f5ce0c92be76113708c1c
Hari Setiaji <hari.setiaji2000@gmail.com>
2023-02-19 21:19:31 +0700

    Initial commit

---
+++ src/main/java/com/setianjay/springboot/learnspringbootjpaandhibernate/LearnSpringBootJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.setianjay.springboot.learnspringbootjpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setianjay/springboot/learnspringbootjpaandhibernate/LearnSpringBootJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setianjay.springboot.learnspringbootjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
